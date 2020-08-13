bfb0dfa2e43ed5d7a4d1a0e0c73334c184a2569d
an81233 <69644349+an81233@users.noreply.github.com>
2020-08-13 09:57:56 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My favorite hobbies to do are crafts, cooking, reading, and gaming.");
+		System.out.println("My favorite games are League of Legends, Roblox, and Osu.");
+		System.out.println("I would like to go to SJSU as my baseline, but UCLA and UCSF sounds good to me, too, that is if I can make it. I highley doubt it.");
+		System.out.println("I am considering majoring in software engineering. Data science, or anything that'll give me a lot of money.");
+		System.out.println("I have some experience in html, lua c, and very recently, python. Java is something I was considering trying out on my own time, but I can not find a website that allowed me to do Java and follow checkpoints/goals to truly understand java.");
+	}
+}
